ee749887761b88fd5e9f0d62260569bb9c8e095c
Rafael <rafael@homeloanexperts.com.au>
2015-09-23 15:02:53 +1000

    Added javascript code to the file

--- README.js
+++ README.js
@@ -1,1 +1,10 @@
-Test file from Raf
+// Test file from Raf
+
+(function($) {
+    $.fn.goTo = function() {
+        $('html, body').animate({
+            scrollTop: $(this).offset().top + 'px'
+        }, 'fast');
+        return this; // for chaining...
+    }
+})(jQuery);
